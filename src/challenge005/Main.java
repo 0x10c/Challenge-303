70736a04d0fb941c46bfa883246f94033f3ab39c
Adrian <iamnowhereonly@gmail.com>
2017-08-17 19:01:05 +0200

    Add 004 and 005

---
+++ src/challenge005/Main.java
@@ -0,0 +1,30 @@
+package challenge005;
+
+public class Main {
+	public static void main(String[] args) {
+		int i, k = 0, j = 0, sum = 0;
+		
+		System.out.println("With for loop: ");
+		for(i = 0; i <= 50; i++) {
+			System.out.println(i);
+		}
+		System.out.println();
+		
+		System.out.println("With do while: ");
+		do {
+			System.out.println(j);
+			j++;
+		} while (j <= 50);
+		System.out.println();
+		
+		System.out.println("With while loop: ");
+		while(k <= 50) {
+			System.out.println(k);
+			sum = sum + k;
+			k++;
+		}
+		
+		System.out.println();
+		System.out.println("Suma wszystkich liczb: " + sum);
+	}
+}
