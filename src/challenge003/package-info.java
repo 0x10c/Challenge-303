30a8d1c66f189126c327bad002be371ebdb88bea
Adrian <iamnowhereonly@gmail.com>
2017-08-17 18:58:57 +0200

    Add "Guess the number" console game

---
+++ src/challenge003/package-info.java
@@ -0,0 +1,8 @@
+/*
+ * @author http://github.com/0x10c
+ * This package is part of project 'Challenge-303'. Read more: https://github.com/0x10c/Challenge-303/blob/master/README.md
+ * 
+ * Java code to calculate area of rectangle
+ */
+
+package challenge003;
