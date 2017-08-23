cddabc01f9e45a6c480ea081ac6f079e3b716a19
Adrian <iamnowhereonly@gmail.com>
2017-08-23 16:44:51 +0200

    Revert "Refactoring 009"
    This reverts commit 9fc5ff20f879c493c05b161ef4511f0dce206d51.

--- src/challenge009/Main.java
+++ src/challenge009/Main.java
@@ -2,28 +2,28 @@ package challenge009;
 
 public class Main {
 	public static void main(String[] args) {
-		int matrix[][] = new int[10][10];
+		int macierz[][] = new int[10][10];
 		int sum, i = 0, j = 0;
 		
 		for(i = 0; i < 10; i++) {
 			for(j = 0; j < 10; j++) {
-				if(i == j)
-					matrix[i][j] = 1;
+				if(i == j) 
+					macierz[i][j] = 1;
 				else
-					matrix[i][j] = 0;
+					macierz[i][j] = 0;
 			}
 		}
 		
 		for(i = 0; i < 10; i++) {
 			for(j = 0; j < 10; j++) {
-				System.out.println(matrix[i][j] + " ");
+				System.out.println(macierz[i][j] + " ");
 			}
 		}
 		
 		sum = 0;
 		for(i = 0; i < 10; i++) {
-			sum = sum + matrix[i][i];
+			sum = sum + macierz[i][i];
 		}
-		System.out.println("Sum of all elements: " + sum);
+		System.out.println("Suma elementów: " + sum);
 	}
 }
