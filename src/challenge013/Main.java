d48f98ad2e0a30c6470cc70dca335b0b60b64707
Adrian <iamnowhereonly@gmail.com>
2017-10-02 17:19:47 +0200

    Add 013

---
+++ src/challenge013/Main.java
@@ -0,0 +1,19 @@
+package challenge013;
+
+class MultiplicationTable {
+    public static void main(String[] args) {
+        int columns, lines = 1;
+
+        System.out.println("Multiplication Table 1 - 100:\n ");
+        do {
+            columns = 1;
+            do {
+                System.out.print(columns * lines + "\t");
+                columns++;
+            }
+            while(columns <= 10);
+            lines++;
+        }
+        while(lines <= 10);
+    }
+}
