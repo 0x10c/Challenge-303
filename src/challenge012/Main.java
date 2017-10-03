9eebc867972b29d98d1941ca2bb5b47e6be9db6c
Adrian <iamnowhereonly@gmail.com>
2017-10-03 17:06:33 +0200

    Update 012

--- src/challenge012/Main.java
+++ src/challenge012/Main.java
@@ -3,12 +3,12 @@ package challenge012;
 
 class MultiplicationTable {
     public static void main(String args[]) {
-        int n = 10, columns, lines;
+        int columns, lines;
 
         System.out.println("Multiplication Table 1 - 100:\n ");
 
-        for(lines = 1; lines <= n; lines++) {
-            for(columns = 1; columns <= n; columns++) {
+        for(lines = 1; lines <= 10; lines++) {
+            for(columns = 1; columns <= 10; columns++) {
                 System.out.print(lines * columns + "\t");
             }
         }
