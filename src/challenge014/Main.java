d1372e3663b555b6875bef1e047c9db977821e43
Adrian <iamnowhereonly@gmail.com>
2017-10-03 17:07:39 +0200

    Add 014

---
+++ src/challenge014/Main.java
@@ -0,0 +1,23 @@
+package challenge014;
+
+public class Main {
+    public static void main(String[] args) {
+        int i, j, n = 10;
+        int matrix[][]= new int[n][n];
+
+        for(i = 0; i < n; i++) {
+            for(j = 0; j < n; j++) {
+                if(j == 0) matrix[i][j] = i;
+                if(j == 1) matrix[i][j] = i * i;
+                if(j > 1) matrix[i][j] = 0;
+            }
+        }
+        // Printing
+        for(i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                System.out.print(matrix[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
