dccab8c8645884cdfbe419085c9210b8003adb6b
theacid <boulderdash@outlook.com>
2017-11-02 20:58:20 +0100

    Refactoring 001. Replacing BufferedReader to Scanner for input data

--- src/challenge001/Main.java
+++ src/challenge001/Main.java
@@ -1,18 +1,19 @@
 package challenge001;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 class ModValue {
     public static void main(String[] args) throws IOException{
         double firstValue, secondValue, result;
+        Scanner inputValues = new Scanner(System.in);
 
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter first value: ");
-        firstValue = Double.parseDouble(reader.readLine());
+        firstValue = Double.parseDouble(inputValues.nextLine());
+
         System.out.println("Enter a second value: ");
-        secondValue = Double.parseDouble(reader.readLine());
+        secondValue = Double.parseDouble(inputValues.nextLine());
+
         result = firstValue%secondValue;
 
         System.out.println("The rest of the division from: " + firstValue + " to " + secondValue + " is: " + result);
