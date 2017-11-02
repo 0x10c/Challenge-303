161b44a4f46086e5c3e3dd8f574bbcfb181bd0b8
theacid <boulderdash@outlook.com>
2017-11-02 16:26:34 +0100

    Add 020

--- src/challenge020/Main.java
+++ src/challenge020/Main.java
@@ -1,40 +1,32 @@
 package challenge020;
 
+import java.util.Scanner;
+
+// compare two int 
+// rename package number to lower
 public class Main {
-    public static void main(String[] args) {
-        String words[] = {"Adrian", "John", "Michael", "Jonathan", "Ana", "monique"};
-        int numbers[] = {0, 7, 3, 23, 6, 8, 10, 11, 34, 10, 5, 4};
-        int a, b, e;
-        int wordsToSort = 5;
-        int numbersToSort = 12;
-
-        System.out.println("Words before sorting ");
-        for(int i = 0; i < wordsToSort; i++) {
-            System.out.print(words[i] + ", ");
-        }
-
-        System.out.println("\nNumbers before sorting: ");
-        for(int i = 0; i < numbersToSort; i++) {
-            System.out.print(numbers[i] + ", ");
-        }
-
-        // Sortowanie
-//        for(a = 1; a < wordsToSort; a++) {
-//            for(b = wordsToSort - 1; b >= a; b--) {
-//                if(words[b-1] > words[b]) {
-//
-//                }
-//            }
-//        }
-
-        System.out.println("Words after sorting: ");
-        for(int i = 0; i < wordsToSort; i++) {
-            System.out.print(words[i] + ", ");
-        }
-
-        System.out.println("\nNumbers after sorting: ");
-        for(int i = 0; i < numbersToSort; i++) {
-            System.out.print(numbers[i] + ", ");
-        }
-    }
+	public static void main(String[] args) {
+		int firstNumber, secondNumber;
+		@SuppressWarnings("resource")
+		Scanner putValue = new Scanner(System.in);
+		
+		System.out.print("Enter a first value: ");
+		firstNumber = putValue.nextInt();
+		
+		System.out.print("Enter a second value: ");
+		secondNumber = putValue.nextInt();
+		
+		if(firstNumber == secondNumber) {
+			System.out.println(firstNumber + " = " + secondNumber);
+		}
+		else if (firstNumber > secondNumber) {
+			System.out.println(firstNumber + " > " + secondNumber);
+		}
+		else if(firstNumber < secondNumber) {
+			System.out.println(firstNumber + " < " + secondNumber);
+		}
+		else {
+			System.out.println(firstNumber + " != " + secondNumber);
+		}
+	}
 }
