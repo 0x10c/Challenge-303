044b8bdc3d38a7fe4de57db6a94265b4301da6dc
theacid <boulderdash@outlook.com>
2017-11-03 16:16:41 +0100

    Add 023

---
+++ src/challenge023/Main.java
@@ -0,0 +1,26 @@
+package challenge023;
+
+import java.util.Scanner;
+
+public class Main {
+	public static boolean result(int a, int b, int c) {
+		return ((a + b) == c || (b + c) == a || (a + c) == b);
+	}
+	
+	public static void main(String[] args) {
+		int i, j, k;
+		Scanner putValue = new Scanner(System.in);
+		
+		System.out.println("Enter first value: ");
+		i = putValue.nextInt();
+		
+		System.out.println("Enter second value: ");
+		j = putValue.nextInt();
+		
+		System.out.print("Enter third value: ");
+		k = putValue.nextInt();
+		
+		System.out.println("Sum for " + result(i, j, k));
+		System.out.print("\n");
+	}
+}
