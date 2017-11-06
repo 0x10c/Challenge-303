53ee4ca107ac1bb45c5383f7586f4d8367ad4d91
theacid <boulderdash@outlook.com>
2017-11-06 23:22:03 +0100

    Add 020 package info

---
+++ src/challenge020/package-info.java
@@ -0,0 +1,9 @@
+/*
+ * Enter two numbers. Compare them by displaying:
+ * ">" if a is greater than b.
+ * "<" If a is less than b.
+ * "=" If both values are equal.
+ * Read more: https://github.com/theacid/Challenge-303/blob/master/README.md
+ */
+
+package challenge020;
