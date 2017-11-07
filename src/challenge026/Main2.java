cd438f0c25e361b4dd94e539082752167202671c
theacid <boulderdash@outlook.com>
2017-11-07 18:46:43 +0100

    Second version for 026

---
+++ src/challenge026/Main2.java
@@ -0,0 +1,18 @@
+package challenge026;
+
+import java.util.Arrays;
+import java.util.IntSummaryStatistics;
+
+public class Main2 {
+    public static void main(String[] args) {
+        int elements[] = { -3, 5, 10, 17, 8, 23, 44, 52 };
+        System.out.println("Original array: " + Arrays.toString(elements));
+
+        IntSummaryStatistics statistics = Arrays.stream(elements).summaryStatistics();
+        int smallestElement = statistics.getMin();
+        int highestElement = statistics.getMax();
+
+        System.out.println("Smallest element: " + smallestElement);
+        System.out.println("Highest element: " + highestElement);
+    }
+}
