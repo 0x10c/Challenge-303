1a9ac91ac6fdac9e303150b58304076d57ef7a55
theacid <boulderdash@outlook.com>
2017-11-07 16:28:09 +0100

    Add 026

---
+++ src/challenge026/Main1.java
@@ -0,0 +1,18 @@
+package challenge026;
+
+// Get the larger value between first and last element of an array of integers
+
+import java.util.Arrays;
+
+public class Main1 {
+    public static void main(String[] args) {
+        int elements[] = { -3, 5, 10, 17, 8, 23, 44, 52 };
+        System.out.println("Original array: " + Arrays.toString(elements));
+
+        int smallestElement = Arrays.stream(elements).min().getAsInt();
+        int highestElement = Arrays.stream(elements).max().getAsInt();
+
+        System.out.println("Smallest element: " + smallestElement);
+        System.out.println("Highest element: " + highestElement);
+    }
+}
