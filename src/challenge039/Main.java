eee81cc70b16a31ae25f43dd82b1d67ae106c169
theacid <boulderdash@outlook.com>
2017-11-16 19:45:07 +0100

    Add 039

---
+++ src/challenge039/Main.java
@@ -0,0 +1,23 @@
+package challenge039;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+class DateIn {
+    public static void main(String[] args) {
+        Date timeToday = new Date();
+
+        DateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy HH:mm:ss");
+
+        dateFormat.setTimeZone(TimeZone.getTimeZone("America/New_York"));
+        String newYork = dateFormat.format(timeToday);
+
+        dateFormat.setTimeZone(TimeZone.getTimeZone("Europe/Moscow"));
+        String moscow = dateFormat.format(timeToday);
+
+
+        System.out.println("Date in New York: " + newYork + "\nDate in Moscow: " + moscow);
+    }
+}
