26cba82e259dfe631823fedaeed26cd2ea7fbf28
theacid <boulderdash@outlook.com>
2017-11-19 21:27:37 +0100

    Add 047

---
+++ src/challenge047/Main.java
@@ -0,0 +1,25 @@
+package challenge047;
+
+import java.util.Scanner;
+
+class MatrixGrid {
+    private static void matrixGrid(int n, int m) {
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                System.out.print((int)(Math.random() * 2) + " ");
+            }
+            System.out.print("\n");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner inputValue = new Scanner(System.in);
+        System.out.println("Horizontal length: ");
+        int n = inputValue.nextInt();
+        System.out.println("Vertical length: ");
+        int m = inputValue.nextInt();
+
+
+        matrixGrid(n,m);
+    }
+}
