a2ee9a93863739deea3cffda47f4dfce8b020bb0
theacid <boulderdash@outlook.com>
2017-11-27 17:27:08 +0100

    Add 064

---
+++ src/challenge064/Main.java
@@ -0,0 +1,14 @@
+package challenge064;
+
+import java.io.File;
+import java.util.Date;
+
+class ModifiedDate {
+    public static void main(String[] args) {
+        File fileModified = new File("/home/theacid/go1.9.1.linux-amd64.tar.gz");
+//        File fileModified = new File("directory_name/file_name");
+
+        Date dateModified = new Date(fileModified.lastModified());
+        System.out.println(dateModified);
+    }
+}
