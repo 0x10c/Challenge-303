3d0c5595616440a75150f3970bbee15d733c529b
theacid <boulderdash@outlook.com>
2017-11-29 14:41:04 +0100

    Add 067

---
+++ src/challenge067/Main.java
@@ -0,0 +1,29 @@
+package challenge067;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+class ReadLineByLine {
+    public static void main(String[] args) {
+        BufferedReader bufferedReader;
+        String line;
+
+        try {
+            bufferedReader = new BufferedReader(new FileReader("/home/theacid/test.txt"));
+            while((line = bufferedReader.readLine()) != null) {
+                System.out.println(line);
+            }
+            bufferedReader.close();
+        }
+
+        catch(FileNotFoundException exc) {
+            System.out.println("Sorry. File not found.");
+        }
+
+        catch(IOException exc) {
+            System.out.println("Sorry. Cannot read file.");
+        }
+    }
+}
