3be62a0159aaa6d4914e87808bf7e7b99b52df87
theacid <boulderdash@outlook.com>
2017-11-29 23:15:48 +0100

    Add package-info

---
+++ src/challenge101/package-info.java
@@ -0,0 +1,6 @@
+/*
+ * Create ArrayList, iterate all elements of array list and print them.
+ * Read more: https://github.com/theacid/Challenge-303/blob/master/README.md
+ */
+
+package challenge101;
