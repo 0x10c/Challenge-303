e82cc5d34d15f0fe4540d34e591a554e4c95c47f
theacid <boulderdash@outlook.com>
2017-11-29 21:44:54 +0100

    Add 100

---
+++ src/challenge100/Main.java
@@ -0,0 +1,19 @@
+package challenge100;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> listOfElements = new ArrayList<>();
+        listOfElements.add("Java");
+        listOfElements.add("Scala");
+        listOfElements.add("Golang");
+        listOfElements.add("C#");
+        listOfElements.add("C");
+        listOfElements.add("C++");
+        listOfElements.add("Elixir");
+
+        System.out.println(listOfElements);
+    }
+}
