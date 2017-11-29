5ab1f8d2deb8d689ff64586965779016d870960b
theacid <boulderdash@outlook.com>
2017-11-29 21:45:33 +0100

    Add 102

---
+++ src/challenge102/Main.java
@@ -0,0 +1,27 @@
+package challenge102;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> listOfElements = new ArrayList<>();
+        listOfElements.add("Java");
+        listOfElements.add("Scala");
+        listOfElements.add("Golang");
+        listOfElements.add("C#");
+        listOfElements.add("C");
+        listOfElements.add("C++");
+        listOfElements.add("Elixir");
+
+        System.out.println(listOfElements);
+
+        listOfElements.add(0, "Malbolge");
+        listOfElements.remove(4);
+
+        Collections.sort(listOfElements);
+
+        System.out.println(listOfElements);
+    }
+}
