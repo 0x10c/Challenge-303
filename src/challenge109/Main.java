df0b59e11d564d7baf2493f5eb6e4dd8a379b9f3
theacid <boulderdash@outlook.com>
2017-11-30 18:29:55 +0100

    Add 109

---
+++ src/challenge109/Main.java
@@ -0,0 +1,19 @@
+package challenge109;
+
+import java.util.ArrayList;
+
+class CloneArrayList {
+    public static void main(String[] args) {
+        ArrayList<String> listOfElements2 = new ArrayList<String>();
+        listOfElements2.add("Spring");
+        listOfElements2.add("JEE");
+        listOfElements2.add("Golang");
+        listOfElements2.add("ASP.NET");
+
+        System.out.println("Before cloning: " + listOfElements2);
+
+        ArrayList<String> clonedArrayList = (ArrayList<String>)listOfElements2.clone();
+
+        System.out.println("After clonning: " + clonedArrayList);
+    }
+}
