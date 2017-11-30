ad152a506f5e9dbb5e6c264934907e1ba976d4bb
theacid <boulderdash@outlook.com>
2017-11-30 21:30:04 +0100

    Add 111

---
+++ src/challenge111/Main.java
@@ -0,0 +1,32 @@
+package challenge111;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class ArrayListJoin {
+    public static void main(String[] args) {
+        List<String> listOfElements = new ArrayList<>();
+        listOfElements.add("Java");
+        listOfElements.add("Scala");
+        listOfElements.add("Golang");
+        listOfElements.add("C#");
+        listOfElements.add("C");
+        listOfElements.add("C++");
+        listOfElements.add("Elixir");
+
+        System.out.println("First array: " + listOfElements);
+
+        listOfElements.remove(5);
+        System.out.println("First array: " + listOfElements);
+
+        listOfElements.removeAll(listOfElements);
+        System.out.println("First array: " + listOfElements);
+
+        if(listOfElements.isEmpty()) {
+            System.out.println("Array is empty");
+        }
+        else {
+            System.out.println("Ok! Keep working!");
+        }
+    }
+}
---
+++ src/challenge111/package-info.java
@@ -0,0 +1,6 @@
+/*
+ * Check if ArrayList is empty.
+ * Read more: https://github.com/theacid/Challenge-303/blob/master/README.md
+ */
+
+package challenge111;
