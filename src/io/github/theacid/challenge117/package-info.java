fd71ef093649e1f324efed445a5540e326700aa1
theacid <boulderdash@outlook.com>
2017-12-03 21:14:36 +0100

    Add 117

---
+++ src/io/github/theacid/challenge117/package-info.java
@@ -0,0 +1,6 @@
+/*
+ * Add element to the LinkedList on the first, last and some random position.
+ * Read more: https://github.com/theacid/Challenge-303/blob/master/README.md
+ */
+
+package io.github.theacid.challenge117;
