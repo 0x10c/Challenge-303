37a98dba9f6d90b2c6987014f737549ba71311dc
theacid <boulderdash@outlook.com>
2017-12-04 00:56:07 +0100

    Add package-info to 051

---
+++ src/io/github/theacid/challenge051/package-info.java
@@ -0,0 +1,6 @@
+/*
+ * Count the occurrences of the values in the array.
+ * Read more: https://github.com/theacid/Challenge-303/blob/master/README.md
+ */
+
+package io.github.theacid.challenge051;
