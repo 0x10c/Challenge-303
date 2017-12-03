232768c7b8645c4ee044df8ec70999fc96e7f84f
theacid <boulderdash@outlook.com>
2017-12-03 22:15:28 +0100

    Add 119

---
+++ src/io/github/theacid/challenge119/Main.java
@@ -0,0 +1,20 @@
+package io.github.theacid.challenge119;
+
+import java.util.LinkedList;
+
+class Main {
+    public static void main(String[] args) {
+        LinkedList<String> listOfElements = new LinkedList<>();
+        listOfElements.add("Java");
+        listOfElements.add("Scala");
+        listOfElements.add("Golang");
+        listOfElements.add("C#");
+        listOfElements.add("C");
+        listOfElements.add("C++");
+        listOfElements.add("Elixir");
+
+        for(int i = 0; i < listOfElements.size(); i++) {
+            System.out.println(i + ". " + listOfElements.get(i));
+        }
+    }
+}
---
+++ src/io/github/theacid/challenge119/package-info.java
@@ -0,0 +1,6 @@
+/*
+ * Display positions and elements from LinkedList
+ * Read more: https://github.com/theacid/Challenge-303/blob/master/README.md
+ */
+
+package io.github.theacid.challenge119;
