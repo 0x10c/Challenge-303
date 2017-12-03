40c7cb6a954c4315d360a756d5673c04e0bffc5b
theacid <boulderdash@outlook.com>
2017-12-04 00:50:43 +0100

    Add 120

---
+++ src/io/github/theacid/challenge120/package-info.java
@@ -0,0 +1,6 @@
+/*
+ * Remove first, last and random value from LinkedList
+ * Read more: https://github.com/theacid/Challenge-303/blob/master/README.md
+ */
+
+package io.github.theacid.challenge120;
