1031a17af6d74a4318b62b6145e12f7b43553b7a
theacid <boulderdash@outlook.com>
2017-12-04 19:36:02 +0100

    Add 124

---
+++ src/io/github/theacid/challenge124/Main.java
@@ -0,0 +1,23 @@
+package io.github.theacid.challenge124;
+
+
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<String> listOfElements = new LinkedList<>();
+        listOfElements.add("Java");
+        listOfElements.add("Scala");
+        listOfElements.add("Golang");
+        listOfElements.add("C#");
+        listOfElements.add("C");
+        listOfElements.add("C++");
+        listOfElements.add("Elixir");
+
+        System.out.println("Original: " + listOfElements);
+        String peekFirst = listOfElements.peekFirst();
+        String peekLast = listOfElements.peekLast();
+
+        System.out.println("First: " + peekFirst + "\nLast: " + peekLast);
+    }
+}
