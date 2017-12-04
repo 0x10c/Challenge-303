86a9860318b1bb81500b96e3bfa02017b04ac4be
theacid <boulderdash@outlook.com>
2017-12-04 19:20:33 +0100

    Add 123

---
+++ src/io/github/theacid/challenge123/Main.java
@@ -0,0 +1,22 @@
+package io.github.theacid.challenge123;
+
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<String> listOfElements = new LinkedList<>();
+        listOfElements.add("Java");
+        listOfElements.add("Scala");
+        listOfElements.add("Golang");
+        listOfElements.add("C#");
+        listOfElements.add("C");
+        listOfElements.add("C++");
+        listOfElements.add("Elixir");
+
+        System.out.println("Original: " + listOfElements);
+
+        LinkedList<String> clone = (LinkedList)listOfElements.clone();
+
+        System.out.print("Clone: " + clone);
+    }
+}
---
+++ src/io/github/theacid/challenge123/package-info.java
@@ -0,0 +1,6 @@
+/*
+ * Clone LinkedList to other
+ * Read more: https://github.com/theacid/Challenge-303/blob/master/README.md
+ */
+
+package io.github.theacid.challenge123;
