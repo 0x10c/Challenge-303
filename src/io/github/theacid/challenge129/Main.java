cde679bb966d6f3ebaab9e14e33d6f857d1595fe
theacid <boulderdash@outlook.com>
2017-12-04 21:20:33 +0100

    Add 129

---
+++ src/io/github/theacid/challenge129/Main.java
@@ -0,0 +1,21 @@
+package io.github.theacid.challenge129;
+
+import java.util.LinkedList;
+
+class CheckIfEmpty {
+    public static void main(String[] args) {
+        LinkedList<String> listOfElements = new LinkedList<>();
+        listOfElements.add("Java");
+        listOfElements.add("Scala");
+        listOfElements.add("Golang");
+        listOfElements.add("C#");
+        listOfElements.add("C");
+        listOfElements.add("C++");
+        listOfElements.add("Elixir");
+
+        System.out.println("Before: " + listOfElements);
+        listOfElements.set(4, "Rust");
+
+        System.out.println("After: " + listOfElements);
+    }
+}
---
+++ src/io/github/theacid/challenge129/package-info.java
@@ -0,0 +1,6 @@
+/*
+ * Replace element on LinkedList
+ * Read more: https://github.com/theacid/Challenge-303/blob/master/README.md
+ */
+
+package io.github.theacid.challenge129;
