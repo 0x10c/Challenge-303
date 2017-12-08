7ed92e502108e87f30b26ef649bacc87287c24be
theacid <boulderdash@outlook.com>
2017-12-08 20:40:29 +0100

    Add 127

---
+++ src/io/github/theacid/challenge127/Main.java
@@ -0,0 +1,35 @@
+package io.github.theacid.challenge127;
+
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList<String> listOfElements = new LinkedList<>();
+        listOfElements.add("Java");
+        listOfElements.add("Scala");
+        listOfElements.add("Golang");
+        listOfElements.add("C#");
+        listOfElements.add("C");
+        listOfElements.add("C++");
+        listOfElements.add("Elixir");
+
+        LinkedList<String> listOfElements2 = new LinkedList<>();
+        listOfElements2.add("ASP.NET");
+        listOfElements2.add("JEE");
+        listOfElements2.add("Golang");
+        listOfElements2.add("Spring");
+        listOfElements2.add("C");
+        listOfElements2.add("C++");
+        listOfElements2.add("Elixir");
+
+        // System.out.println(listOfElements.containsAll(listOfElements2));
+        for(String elements : listOfElements) {
+            if(listOfElements2.contains(elements)) {
+                System.out.println("Yes");
+            }
+            else {
+                System.out.println("No");
+            }
+        }
+    }
+}
---
+++ src/io/github/theacid/challenge127/package-info.java
@@ -0,0 +1,6 @@
+/*
+ * Compare two LinkedList
+ * Read more: https://github.com/theacid/Challenge-303/blob/master/README.md
+ */
+
+package io.github.theacid.challenge127;
