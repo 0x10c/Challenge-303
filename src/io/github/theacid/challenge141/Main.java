ce832e2f65c25a5e731bfb033914c0d069c45e00
theacid <boulderdash@outlook.com>
2017-12-08 16:01:04 +0100

    Add 141

---
+++ src/io/github/theacid/challenge141/Main.java
@@ -0,0 +1,19 @@
+package io.github.theacid.challenge141;
+
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        TreeSet<String> listOfElements = new TreeSet<>();
+        listOfElements.add("Java");
+        listOfElements.add("Scala");
+        listOfElements.add("Golang");
+        listOfElements.add("C#");
+        listOfElements.add("C");
+        listOfElements.add("C++");
+        listOfElements.add("Elixir");
+
+        System.out.println(listOfElements);
+
+    }
+}
