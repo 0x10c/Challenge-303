216bc3d0a5ee4bc11bd508f07763fa911684acbc
theacid <boulderdash@outlook.com>
2017-12-08 16:07:01 +0100

    Add 143

---
+++ src/io/github/theacid/challenge143/Main.java
@@ -0,0 +1,18 @@
+package io.github.theacid.challenge143;
+
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        TreeSet<String> listOfElements = new TreeSet<>();
+        listOfElements.add("Java");
+        listOfElements.add("Scala");
+        listOfElements.add("Golang");
+        listOfElements.add("C#");
+        listOfElements.add("C");
+        listOfElements.add("C++");
+        listOfElements.add("Elixir");
+
+        System.out.println("List of elements: " + listOfElements.size());
+    }
+}
---
+++ src/io/github/theacid/challenge143/package-info.java
@@ -0,0 +1,6 @@
+/*
+ * Get the number of elements from HashSet
+ * Read more: https://github.com/theacid/Challenge-303/blob/master/README.md
+ */
+
+package io.github.theacid.challenge143;
