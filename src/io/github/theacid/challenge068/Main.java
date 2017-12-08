85c207d87f56ab1fa2dadcb179cb8fc4adfabbb5
theacid <boulderdash@outlook.com>
2017-12-08 21:08:03 +0100

    Add 068

---
+++ src/io/github/theacid/challenge068/Main.java
@@ -0,0 +1,29 @@
+package io.github.theacid.challenge068;
+
+import java.io.*;
+
+class ReadLineByLine {
+    public static void main(String[] args) {
+        String line = "";
+
+        try {
+            String file = "/home/theacid/test.txt";
+            FileWriter fileWriter = new FileWriter(file, true);
+            fileWriter.write("New text");
+            fileWriter.close();
+
+            BufferedReader bufferedReader = new BufferedReader(new FileReader("home/theacid/test.txt"));
+            while(line != null) {
+                StringBuilder stringBuilder = null;
+                stringBuilder.append(line);
+                line = bufferedReader.readLine();
+                System.out.println(line);
+            }
+
+            bufferedReader.close();
+        }
+        catch(IOException exc) {} {
+            System.out.println("Error");
+        }
+    }
+}
---
+++ src/io/github/theacid/challenge068/package-info.java
@@ -0,0 +1,6 @@
+/*
+ * Add text to existing file
+ * Read more: https://github.com/theacid/Challenge-303/blob/master/README.md
+ */
+
+package io.github.theacid.challenge068;
