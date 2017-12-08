4af7e551bef34b082e58b14b3cddfd91f42d7b42
theacid <boulderdash@outlook.com>
2017-12-08 17:11:22 +0100

    Add 144

---
+++ src/io/github/theacid/challenge144/Main.java
@@ -0,0 +1,32 @@
+package io.github.theacid.challenge144;
+
+import java.util.TreeSet;
+
+public class Main {
+    public static void main(String[] args) {
+        TreeSet<String> listOfElements = new TreeSet<>();
+        listOfElements.add("Java");
+        listOfElements.add("Scala");
+        listOfElements.add("Golang");
+        listOfElements.add("C#");
+        listOfElements.add("C");
+        listOfElements.add("C++");
+        listOfElements.add("Elixir");
+
+        System.out.println("First set: " + listOfElements);
+
+
+        TreeSet<String> listOfElements2 = new TreeSet<>();
+        listOfElements2.add("ASP.NET");
+        listOfElements2.add("JEE");
+        listOfElements2.add("Spring");
+        listOfElements2.add("Iris MVC");
+
+        System.out.println("Second set: " + listOfElements2);
+
+
+        listOfElements.addAll(listOfElements2);
+        System.out.println("All elements in one: "  + listOfElements);
+
+    }
+}
---
+++ src/io/github/theacid/challenge144/package-info.java
@@ -0,0 +1,6 @@
+/*
+ * Move all elements from one TreeSet to other and print all them
+ * Read more: https://github.com/theacid/Challenge-303/blob/master/README.md
+ */
+
+package io.github.theacid.challenge144;
