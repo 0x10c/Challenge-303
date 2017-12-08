52d610463e3a6b31461d06f386b5bf02e4ffc4ce
theacid <boulderdash@outlook.com>
2017-12-08 11:44:29 +0100

    Add 140

---
+++ src/io/github/theacid/challenge140/Main.java
@@ -0,0 +1,27 @@
+package io.github.theacid.challenge140;
+
+import java.util.LinkedHashSet;
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedHashSet<String> listOfElements = new LinkedHashSet<>();
+        listOfElements.add("Java");
+        listOfElements.add("Scala");
+        listOfElements.add("Golang");
+        listOfElements.add("C#");
+        listOfElements.add("C");
+        listOfElements.add("C++");
+        listOfElements.add("Elixir");
+
+        System.out.println("Before: " + listOfElements);
+        listOfElements.clear();
+
+        if(listOfElements.isEmpty()) {
+            System.out.println("List empty.");
+        }
+        else {
+            System.out.println("Ok!");
+        }
+
+    }
+}
---
+++ src/io/github/theacid/challenge140/package-info.java
@@ -0,0 +1,6 @@
+/*
+ * Remove all elements from HashSet and check if it's empty
+ * Read more: https://github.com/theacid/Challenge-303/blob/master/README.md
+ */
+
+package io.github.theacid.challenge140;
