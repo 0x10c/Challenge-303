aef7a8941082d8ac9e623868250a767009e81866
theacid <boulderdash@outlook.com>
2017-12-11 14:22:49 +0100

    Add 152

---
+++ src/io/github/theacid/challenge152/Main.java
@@ -0,0 +1,18 @@
+package io.github.theacid.challenge152;
+
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap<Integer, String> listOfElements = new HashMap<>();
+        listOfElements.put(1, "Java");
+        listOfElements.put(2, "Scala");
+        listOfElements.put(3, "Golang");
+        listOfElements.put(4, "C#");
+        listOfElements.put(5, "C");
+        listOfElements.put(6, "C++");
+        listOfElements.put(7, "Elixir");
+
+        System.out.println("HashMap has: " + listOfElements.size() + " elements.");
+    }
+}
---
+++ src/io/github/theacid/challenge152/package-info.java
@@ -0,0 +1,6 @@
+/*
+ * Count elements in HashMap
+ * Read more: https://github.com/theacid/Challenge-303/blob/master/README.md
+ */
+
+package io.github.theacid.challenge152;
