e948d457346fb43782c17700a58877a3930aacff
theacid <boulderdash@outlook.com>
2017-12-13 19:00:34 +0100

    Add 157

---
+++ src/io/github/theacid/challenge158/Main.java
@@ -0,0 +1,25 @@
+package io.github.theacid.challenge158;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+public class Main {
+    public static void main(String[] args) {
+        TreeMap<Integer, String> listOfElements = new TreeMap<>();
+        listOfElements.put(1, "Java");
+        listOfElements.put(2, "Scala");
+        listOfElements.put(3, "Golang");
+        listOfElements.put(4, "C#");
+        listOfElements.put(5, "C");
+        listOfElements.put(6, "C++");
+        listOfElements.put(7, "Elixir");
+
+        System.out.println("TreeSet values: " + listOfElements);
+
+        // or
+
+        for(Map.Entry<Integer, String> newEntry : listOfElements.entrySet()) {
+            System.out.println(newEntry.getKey() + ". " + newEntry.getValue());
+        }
+    }
+}
---
+++ src/io/github/theacid/challenge158/package-info.java
@@ -0,0 +1,6 @@
+/*
+ * Create TreeMap, add some elements and print them
+ * Read more: https://github.com/theacid/Challenge-303/blob/master/README.md
+ */
+
+package io.github.theacid.challenge158;
