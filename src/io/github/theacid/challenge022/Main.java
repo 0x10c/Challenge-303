16ad80e40c9c358899ef4529a9748e6bbf3f433c
theacid <boulderdash@outlook.com>
2018-02-05 12:32:56 +0100

    Update 022

--- src/io/github/theacid/challenge022/Main.java
+++ src/io/github/theacid/challenge022/Main.java
@@ -1,26 +1,31 @@
 package io.github.theacid.challenge022;
 
-class DivideBy {
+public class Main {
     public static void main(String[] args) {
- 
+    	// The sum of the numbers in the columne on the right has been added
+		// for divide by 3 and 5
+		int result = 0;
+
     	System.out.println("\nDivided by 3: ");
     	for(int i = 0; i <= 100; i++) {
     		if(i % 3 == 0) {
     			System.out.print(i + " ");
     		}
     	}
-    	
+
     	System.out.println("\nDivided by 5: ");
     	for(int i = 0; i <= 100; i++) {
     		if(i % 5 == 0) {
     			System.out.print(i + " ");
     		}
     	}
-    	
+
     	System.out.println("\nDivided by 3 and 5: ");
-    	for(int i = 0; i <= 100; i++) {
+    	for(int i = 0; i <= 1001; i++) {
     		if(i % 3 == 0 && i % 5 == 0) {
     			System.out.print(i + " ");
+				System.out.print("Sum of previous numbers: ");
+				System.out.println(result += i);
     		}
     	}
     }
