58dde09f3eaf5ad6e74bd949b92b24f8d669981c
theacid <boulderdash@outlook.com>
2018-05-28 17:09:30 +0200

    Update 036th

--- src/main/java/io/github/theacid/challenge036/Main.java
+++ src/main/java/io/github/theacid/challenge036/Main.java
@@ -1,18 +1,30 @@
 package io.github.theacid.challenge036;
 
-class MaxValueArray {
+public class Main {
     private static int numbers[] = {-5, 3, 8, 10, 15, 18, 19, 21, 22, 25, 27, 31, 35, 38, 40, 42, 44, 50 };
 
-    private static int LargestValue() {
+    private static int largestValue() {
         int maxElement = numbers[0];
-        int i;
 
-        for(i = 0; i < numbers.length; i++)
-            if(numbers[i] > maxElement)
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] > maxElement)
                 maxElement = numbers[i];
-            return maxElement;
+        }
+        return maxElement;
     }
+
+    private static int smallestValue() {
+        int minElement = numbers[0];
+
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] < minElement)
+                minElement = numbers[i];
+        }
+        return minElement;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Largest element: " + LargestValue());
+        System.out.println("Largest element: " + largestValue());
+        System.out.println("Smallest element: " + smallestValue());
     }
 }
